9c18fc4fb3b77fca65517c875db1aa4635716228
rupali dudhal <rupalidudhal95@gmail.com>
2025-11-23 20:20:01 +0530

    weather app

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -1,9 +1,11 @@
+import Weather from "./Weather"
+
 function App() {
- return(
-    <div>
-        <Weather/>
-    </div>
- )
+ return (
+   <div style={{ backgroundColor: "rgba(127, 214, 243, 0.2)", height:"100vh" }}>
+     <Weather />
+   </div>
+ );
 }
 
 export default App
--- weather_app/src/Weather.jsx
+++ weather_app/src/Weather.jsx
@@ -1,12 +1,107 @@
 import { useEffect, useState } from "react";
+import "./Weather.css";
 
-export default function Weather(){
-    const [data,setData] = useState('');
+export default function Weather() {
+  const [data, setData] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [cityName, setCityName] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
-        const url = 'https://api.weatherapi.com/v1/current.json';
-         fetch(url).then((res)=>res.json()).then((json)=>setData(json));
-         console.log(data);
-    },[])
-    
+  const key = "4b61d5aa02644b2a9c1120724252311";
+
+  useEffect(() => {
+    if (cityName) {
+      const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${cityName}`;
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((json) => {
+          if (json.error) {
+            throw new Error(json.error.message);
+          }
+          setData(json);
+        })
+        .catch(() => alert("Failed to fetch weather data"))
+        .finally(() => setLoading(false));
+    }
+  }, [cityName]);
+
+  const handleChange = (e) => {
+    const city = e.target.value;
+    if (!city) {
+      setInputValue("");
+      return;
+    }
+    const cityValue = city[0].toUpperCase() + city.slice(1).toLowerCase();
+    // console.log(cityValue, "input Value");
+    setInputValue(cityValue);
+  };
+
+  const handleClick = () => {
+    setCityName(inputValue);
+    setLoading(true);
+  };
+
+  return (
+    <div
+      style={{
+        paddingTop: "200px",
+        textAlign: "center",
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter City Name"
+        onChange={(e) => handleChange(e)}
+        style={{
+          width: "250px",
+          height: "25px",
+          fontSize: "18px",
+          padding: "5px 10px",
+          border: "none",
+        }}
+      />
+      <button
+        style={{
+          width: "120px",
+          height: "40px",
+          fontSize: "18px",
+          backgroundColor: "rgba(22, 113, 22, 1)",
+          cursor: "pointer",
+          border: "none",
+          color: "white",
+        }}
+        onClick={handleClick}
+      >
+        Search
+      </button>
+      {loading && <p>Loading Data...</p>}
+
+      {data && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            paddingTop: "80px",
+          }}
+        >
+          <div className="weather-card">
+            <p>Temperature </p>
+            <p>{data.current.temp_c}</p>
+          </div>
+          <div className="weather-card">
+            <p>Humidity</p>
+            <p>{data.current.humidity}%</p>
+          </div>
+          <div className="weather-card">
+            <p>Condition</p>
+            <p>{data.current.condition.text}</p>
+          </div>
+          <div className="weather-card">
+            <p>Wind Speed</p>
+            <p>{data.current.wind_kph}kph</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
